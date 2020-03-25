51332fb5254b30bfcd01a8e5532ece2e370321b7
xujiqiang <fenchuiyun@gmail.com>
2020-03-25 23:45:45 +0800

    first commit

---
+++ src/main/java/com/fenchuiyun/GitLearn.java
@@ -0,0 +1,11 @@
+package com.fenchuiyun;
+
+/**
+ * Description:
+ *
+ * @author <a href="mailto:fenchuiyun@gmail.com">xujiqiang</a>
+ * @date  Create on 2020年03月25日
+ * @since version1.0 Copyright 2020 CLKJ All Rights Reserved.
+ */
+public class GitLearn {
+}
